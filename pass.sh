6722ef16b992a5d69d4b890d1c0d62b4a99b2e4d
Kamil Zielinski <kamil.zielinski@bluesoft.com>
2020-07-22 18:02:04 +0800

    Add pass.sh

--- pass.sh
+++ pass.sh
@@ -2,10 +2,12 @@
 chars1="qwertyuiopasdfghjklzxcvbnm"
 chars2="QWERTYUIOPASDFGHJKLZXCVBNM"
 chars3="1234567890"
-for i in {1..5} ; do
+chars4="!@#*"
+for i in {1..4} ; do
     pass1=$(echo -n "$pass1${chars1:RANDOM%${#chars1}:1}")
     pass2=$(echo -n "$pass2${chars2:RANDOM%${#chars2}:1}")
     pass3=$(echo -n "$pass3${chars3:RANDOM%${#chars3}:1}")
+    pass4=$(echo -n "$pass4${chars4:RANDOM%${#chars4}:1}")
 done
-password=$(echo "$pass1$pass2$pass3!")
+password=$(echo "$pass1$pass2$pass3$pass4")
 echo $password
